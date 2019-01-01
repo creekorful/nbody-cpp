f36d62c59afa8122cb2a32c69ed8155041ef7b29
Aloïs Micard <lunamicard@gmail.com>
2019-01-01 23:24:34 +0100

    Add sun & earth body

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -7,6 +7,7 @@ Application::Application(int argc, char** argv)
 
 int Application::execute()
 {
-    System system;
+    System system({Body("Sun", 1.98892 * std::pow(10, 30), {}, {}),
+                   Body("Earth", 5.9742 * std::pow(10, 24), {-1*AU, 0}, {0,  29.783 * 1000})});
     return system.simulate();
 }
--- Source/constant.h
+++ Source/constant.h
@@ -1,6 +1,10 @@
 #ifndef NBODY_CPP_CONSTANT_H
 #define NBODY_CPP_CONSTANT_H
 
+#include <cmath>
+
 constexpr double G = 6.67428e-11;
+
+#define AU std::pow(149.6, 6) * 1000
 
 #endif //NBODY_CPP_CONSTANT_H
