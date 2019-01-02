393a734ed805aa2c2ecd641b1ce355bd13320e37
Aloïs Micard <lunamicard@gmail.com>
2019-01-02 11:29:26 +0100

    add venus

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -2,12 +2,12 @@
 
 Application::Application(int argc, char** argv)
 {
-
 }
 
 int Application::execute()
 {
     System system({Body("Sun", 1.98892 * std::pow(10, 30), {}, {}),
-                   Body("Earth", 5.9742 * std::pow(10, 24), {-1*AU, 0}, {0,  29.783 * 1000})});
+                   Body("Earth", 5.9742 * std::pow(10, 24), {-1 * AU, 0}, {0, 29.783 * 1000}),
+                   Body("Venus", 4.8685 * std::pow(10, 24), {0.723 * AU, 0}, {0, -35.02 * 1000})});
     return system.simulate();
 }
