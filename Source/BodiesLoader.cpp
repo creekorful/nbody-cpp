28e95f76139936424b6be9207737a6ffc503b8a0
Aloïs Micard <amicard@eurodns.com>
2019-01-03 12:51:46 +0100

    Display details about body when loading

--- Source/BodiesLoader.cpp
+++ Source/BodiesLoader.cpp
@@ -15,8 +15,6 @@ std::vector<Body> BodiesLoader::loadBodies()
     std::string line;
     while (std::getline(m_file, line))
     {
-        std::cout << line << std::endl;
-
         std::stringstream ssline(line);
         std::vector<std::string> parts;
         std::string part;
@@ -26,10 +24,14 @@ std::vector<Body> BodiesLoader::loadBodies()
             parts.push_back(part);
         }
 
-        bodies.emplace_back(parts[0], std::stod(parts[1]), std::stod(parts[2]),
-                            Vector(std::stod(parts[3]) * AU, std::stod(parts[4]) * AU),
-                            Vector(std::stod(parts[5]), std::stod(parts[6])),
-                            sf::Color(static_cast<sf::Uint32>(std::stoul(parts[7]))));
+        Body body = bodies.emplace_back(parts[0], std::stod(parts[1]), std::stod(parts[2]),
+                                        Vector(std::stod(parts[3]) * AU, std::stod(parts[4]) * AU),
+                                        Vector(std::stod(parts[5]), std::stod(parts[6])),
+                                        sf::Color(static_cast<sf::Uint32>(std::stoul(parts[7]))));
+
+        std::cout << "Loaded body: " << body.name() << ". Position: " << body.position() << " AU. Orbital velocity: "
+                  << body.velocity() << "km/s. Mass: " << body.mass() << "kg. Radius: " << body.radius() << "km."
+                  << std::endl;
     }
 
     return bodies;
