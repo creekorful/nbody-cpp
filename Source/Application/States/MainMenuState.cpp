85b5198b59211a8a327823eccc68da393da592b6
Aloïs Micard <amicard@eurodns.com>
2019-01-08 15:12:43 +0100

    Refactor to use new majoris version

--- Source/Application/States/MainMenuState.cpp
+++ Source/Application/States/MainMenuState.cpp
@@ -3,6 +3,7 @@
 void MainMenuState::initialize()
 {
     // Load font
+    getEngine()->fontManager().loadFont("Fonts/Arial.ttf");
     if (!m_arialFont.loadFromFile("Fonts/Arial.ttf"))
     {
         std::cerr << "Unable to load font" << std::endl;
@@ -10,22 +11,15 @@ void MainMenuState::initialize()
     }
 
     // Get window size
+    sf::Vector2u screenSize = getEngine()->screenSize();
 
-    // Initialize texts
-    m_playText.setFont(m_arialFont);
-    m_playText.setString("Play");
-    m_playText.setFillColor(sf::Color::Yellow);
-    m_playText.setPosition(100, 100);
-
-    m_editorText.setFont(m_arialFont);
-    m_editorText.setString("Editor");
-    m_editorText.setFillColor(sf::Color::Yellow);
-    m_editorText.setPosition(100, 200);
-
-    m_quitText.setFont(m_arialFont);
-    m_quitText.setString("Quit");
-    m_quitText.setFillColor(sf::Color::Yellow);
-    m_quitText.setPosition(100, 300);
+    // Initialize menu
+    m_menu.setDefaultColor(sf::Color::Yellow);
+    m_menu.setHighlightColor(sf::Color::Red);
+    m_menu.setItems({"Play", "Editor", "Quit"}, m_arialFont);
+    m_menu.setPosition(screenSize.x / 2, screenSize.y / 2);
+    m_menu.setCallback(this);
+    add(&m_menu);
 }
 
 std::string MainMenuState::title()
@@ -33,21 +27,34 @@ std::string MainMenuState::title()
     return "MainMenu - [N-Body C++]";
 }
 
-void MainMenuState::update(float dt)
-{
-}
-
-void MainMenuState::draw(sf::RenderTarget& target, sf::RenderStates states) const
-{
-    target.draw(m_playText, states);
-    target.draw(m_editorText, states);
-    target.draw(m_quitText, states);
-}
-
 void MainMenuState::pollEvent(const sf::Event& event)
 {
+    GameState::pollEvent(event);
+
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
     {
         getEngine()->quit();
     }
+}
+
+void MainMenuState::onMenuItemSelected(const std::string& itemName)
+{
+    if (itemName == "Play")
+    {
+
+    }
+
+    else if (itemName == "Editor")
+    {
+
+    }
+
+    else if (itemName == "Quit")
+    {
+        getEngine()->quit();
+    }
+}
+
+void MainMenuState::onMenuItemHovered(const std::string& itemName)
+{
 }
--- Source/Application/States/MainMenuState.h
+++ Source/Application/States/MainMenuState.h
@@ -6,15 +6,19 @@
 #include <SFML/Graphics/Text.hpp>
 
 #include "../../Majoris/Source/Engine/State/GameState.h"
+#include "../../Majoris/Source/Engine/GameObject/Gui/Menu.h"
 
-class MainMenuState : public ma::GameState
+class MainMenuState : public ma::GameState, public ma::Gui::IMenuCallback
 {
 public:
-    void update(float dt) override;
 
     void pollEvent(const sf::Event& event) override;
 
     std::string title() override;
+
+    void onMenuItemSelected(const std::string& itemName) override;
+
+    void onMenuItemHovered(const std::string& itemName) override;
 
 
     static MainMenuState& instance()
@@ -24,15 +28,11 @@ public:
     }
 
 protected:
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-
     void initialize() override;
 
 private:
     sf::Font m_arialFont;
-    sf::Text m_playText;
-    sf::Text m_editorText;
-    sf::Text m_quitText;
+    ma::Gui::Menu m_menu;
 };
 
 
