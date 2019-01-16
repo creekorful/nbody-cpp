af956af9a1fd533220d256d56678f0c6b632339b
Aloïs Micard <amicard@eurodns.com>
2019-01-16 12:47:24 +0100

    Remove old application since new one does the same thing

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,21 +1,7 @@
-#include "Application/OldApplication.h"
 #include "Application/Application.h"
 
 int main(int argc, char** argv)
 {
-    int ret = -1;
-    // if --engine flag is passed switch to new game engine version
-    if (argc > 1 && strcmp(argv[1], "--engine") == 0)
-    {
-        auto application = new Application(argc, argv);
-        ret = application->execute();
-    }
-    // otherwise fallback to old one
-    else
-    {
-        auto application = new OldApplication(argc, argv);
-        ret = application->execute();
-    }
-
-    return ret;
+    auto application = new Application(argc, argv);
+    return application->execute();
 }
